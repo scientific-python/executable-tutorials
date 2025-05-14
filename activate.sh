ec9f315699b7131f9d8b5c14c53ee74ee87a0590
Kyle Sunden <git@ksunden.space>
2025-05-14 18:33:03 -0500

    Fix activation script

--- activate.sh
+++ activate.sh
@@ -1,1 +1,6 @@
-source $(dirname $0)/.binder/postBuild
+#!/usr/bin/env bash
+
+set -eux
+
+mkdir -p ${CONDA_PREFIX}/share/jupyter/lab/settings
+cp ${PIXI_PROJECT_ROOT}/.binder/overrides.json ${CONDA_PREFIX}/share/jupyter/lab/settings/overrides.json
