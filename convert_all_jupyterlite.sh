9aa694c3f58b0381314e82ea5ce21eb7c717b246
Kyle Sunden <git@ksunden.space>
2025-05-14 19:25:32 -0500

    Better comment conversion script

--- convert_all_jupyterlite.sh
+++ convert_all_jupyterlite.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
+# This is a script which copies the contents of the tutorials
+# directory into a build directory (_build/ipynbs).
+# The notebook markdown files are converted to ipynbs with other
+# files (non-executable md files, static images, etc) are copied
+# directly.
+# This is intended for jupyterlite to build pointing to the build
+# directory since the markdown files do not currently work in 
+# jupyterlite.
+
 # Find Markdown files convert.
-all_markdown_files=$(find tutorials -type f)
-if [ $# -gt 0 ]; then
-    files_to_process="$@"
-else
-    files_to_process=$all_markdown_files
-fi
+files_to_process=$(find tutorials -type f)
 
 OUTDIR="_build/ipynbs"
 
 # Identify Markdown files that are Jupytext and convert them all.
 for file in ${files_to_process}; do
+    # Ensure result directory exists
     echo "Making directory: $OUTDIR/$(dirname $file)"
     mkdir -p $OUTDIR/$(dirname $file)
 
     echo loop in $file
     # Extract the kernel information from the Jupytext Markdown file.
     kernel_info=$(grep -A 10 '^---$' "$file" | grep -E 'kernelspec')
-    # Skip if no kernel information was found.
+    # Copy directly if not a notebook file
     if [ -z "$kernel_info" ]; then
       cp $file $OUTDIR/$file
       continue
@@ -26,5 +31,4 @@ for file in ${files_to_process}; do
     # Convert to ipynb format, to be consumed by pytest nbval plugin.
     notebook_file="${file%.md}.ipynb"
     jupytext --to ipynb "$file" --output $OUTDIR/${notebook_file}
-    # Stash file in array to be cleaned up at the end.
 done
